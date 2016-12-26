c9887667d8ee286b175b06f819da777c39051402
CAHL92 <dbrito@3dlinkweb.com>
2016-12-26 15:19:35 -0400

    database

--- Config/Schema/translate.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `personals` ADD `bio_esp` TEXT NOT NULL AFTER `bio`;
-
-ALTER TABLE `quotes` ADD `text_esp` TEXT NOT NULL AFTER `text`;
-
-ALTER TABLE `work` ADD `orden` INT NOT NULL AFTER `name`;
